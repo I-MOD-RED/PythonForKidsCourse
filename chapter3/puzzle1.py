b0df19ff17631724a1c437d1f87423ad05376597
Ivana Modrovicova <modrovic.ivana@gmail.com>
2021-12-07 23:24:49 +0100

    i learned how to use lists
    Signed-off-by: Ivana Modrovicova <modrovic.ivana@gmail.com>

---
+++ chapter3/puzzle1.py
@@ -0,0 +1,11 @@
+games = ["lukostrelba", "kresba", "citanie"]
+foods = ["ryba", "palacinky", "donaty"]
+favorites = games + foods
+print(favorites)
+
+print(3 * 25 + 2 * 40)
+
+name = "Hi there, %s %s!"
+first = "Ivana"
+last = "Modrovic"
+print(name % (first, last))
